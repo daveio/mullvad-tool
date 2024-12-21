6a06615a060f8122ebdf8753876a0fe2edef8738
Dave Williams <dave@dave.io>
2024-12-21 01:56:56 +0000

    cleanup gen-all

--- gen-all.fish
+++ gen-all.fish
@@ -1,1 +1,4 @@
-for i in meta/conf/wireguard-ipv4-51820/*; python mullvad wireguard $i -l (python mullvad portgen run) | tee -a script; end
+#! env fish
+for i in meta/conf/wireguard-ipv4-51820/*
+  python mullvad wireguard $i -l (python mullvad portgen run) | tee -a script
+end
